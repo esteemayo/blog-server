3710239bf7d867c9bf79fc0d92d8b8e9aee27b01
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-30 00:55:59 +0100

    feat: setup the app server

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+/* eslint-disable */
+
 import app from './app.js';
+import connectDB from './config/db.config.js';
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 🔥 Shutting down...'.red.bold);
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+app.set('port', process.env.PORT || 9090);
+
+const server = app.listen(app.get('port'), async () => {
+  await connectDB();
+  console.log(`App listening on port → ${server.address().port}`.cyan.bold);
+});
+
+process.on('unhandleRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 🔥 Shutting down...'.red.bold);
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
