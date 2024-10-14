7df04face9ec874b835e43cb8b54c5fc190f7614
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-14 19:06:51 +0100

    feat: add register and login user route endpoints

---
+++ controllers/auth.controller.js
@@ -0,0 +1,8 @@
+import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+
+import User from '../models/user.models.js';
+
+export const register = asyncHandler(async (req, res, next) => {});
+
+export const login = asyncHandler(async (req, res, next) => {});
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,11 @@
 import express from 'express';
 
+import * as authController from '../controllers/auth.controller.js';
+
 const router = express.Router();
+
+router.post('/register', authController.register);
+
+router.post('/login', authController.login);
 
 export default router;
