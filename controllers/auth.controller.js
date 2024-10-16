0af84e5bb36115e9f99a4e4dd4f336d5c26c37b3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-16 01:46:10 +0100

    feat: handle user login api route logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/user.models.js';
+import BadRequesError from './../errors/bad.request.error.js';
 
 export const register = asyncHandler(async (req, res, next) => {
   const user = await User.create({ ...req.body });
@@ -11,4 +12,18 @@ export const register = asyncHandler(async (req, res, next) => {
   }
 });
 
-export const login = asyncHandler(async (req, res, next) => {});
+export const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new BadRequesError('Plaese provide email and password'));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.comparePassword(password))) {
+    return next(new BadRequesError('Incorrect email or password'));
+  }
+
+  return res.status(StatusCodes.OK).json(user);
+});
