c59cce04cfb5596bc6114985f5178857a14c536e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-17 19:43:13 +0100

    feat: add update current user' password api route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import * as authController from '../controllers/auth.controller.js';
+import * as authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
@@ -13,5 +14,11 @@ router.post('/logout', authController.logout);
 router.post('/forgot-password', authController.forgotPassword);
 
 router.patch('/reset-password/:token', authController.resetPassword);
+
+router.patch(
+  '/update-my-password',
+  authMiddleware.protect,
+  authController.updatePassword,
+);
 
 export default router;
