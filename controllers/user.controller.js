b66234285949444462a0fa29c00414fcff514299
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-17 20:16:01 +0100

    feat: handle update current user' data route logic

---
+++ controllers/user.controller.js
@@ -0,0 +1,51 @@
+import { StatusCodes } from 'http-status-codes';
+import _ from 'lodash';
+import asyncHandler from 'express-async-handler';
+
+import User from '../models/user.model.js';
+
+import CustomAPIError from '../errors/cutom.api.error.js';
+import NotFoundError from '../errors/not.found.error.js';
+import BadRequesError from './../errors/bad.request.error.js';
+
+import { createSendToken } from '../utils/create.send.token.util.js';
+
+export const updateMe = asyncHandler(async (req, res, next) => {
+  const { id: userId } = req.user;
+  const { password, passwordConfirm } = req.body;
+
+  if (password || passwordConfirm) {
+    return next(
+      new BadRequesError(
+        `This route is not for password updates. Please use ${req.protocol}://${req.get('host')}/api/v1/auth/update-my-password`,
+      ),
+    );
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return next(
+      new NotFoundError(`There is no user found with the given ID → ${userId}`),
+    );
+  }
+
+  const filterBody = _.pick(req.body, [
+    'name',
+    'email',
+    'phone',
+    'bio',
+    'image',
+  ]);
+
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    { $set: { ...filterBody } },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  return createSendToken(updatedUser, StatusCodes.OK, req, res);
+});
