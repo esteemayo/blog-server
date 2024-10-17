09de35644769a96ded13486931f70bff4ac18ab5
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-17 20:44:14 +0100

    feat: handle fetch single user api route endpoint logic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,6 +20,20 @@ export const getUsers = asyncHandler(async (req, res, next) => {
   return res.status(StatusCodes.OK).json(users);
 });
 
+export const getUser = asyncHandler(async (req, res, next) => {
+  const { id: userId } = req.params;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return next(
+      new NotFoundError(`There is no user found with the given ID → ${userId}`),
+    );
+  }
+
+  return res.status(StatusCodes.OK).json(user);
+});
+
 export const updateMe = asyncHandler(async (req, res, next) => {
   const { id: userId } = req.user;
   const { password, passwordConfirm } = req.body;
