b1768bff2c3641c5391c7549d99b1b937b4385da
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-17 01:04:33 +0100

    fix: rename file

---
+++ models/user.model.js
@@ -0,0 +1,99 @@
+import bcrypt from 'bcryptjs';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide your name'],
+      trim: true,
+      minLength: [6, 'Your name cannot be less than 6 characters long'],
+      maxLength: [50, 'Your name cannot be more than 50 characters long'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email address'],
+      unique: true,
+      trim: true,
+      lowerCase: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'Please enter a valid email address',
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide your password'],
+      minLength: [8, 'Passwords cannot be less than 8 characters long'],
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password'],
+      validate: {
+        validator: function (val) {
+          return val === this.password;
+        },
+        message: 'Passwords do not match',
+      },
+    },
+    phone: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    fromGoogle: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ['admin', 'user'],
+        message: 'Role is either: admin or user',
+      },
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { id: this._id, email: this.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    },
+  );
+
+  return token;
+};
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
