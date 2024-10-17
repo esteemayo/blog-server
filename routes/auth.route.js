c209c1717c5ad84e5e3e00a61491fa0540306885
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-17 01:43:19 +0100

    feat: add forgot user' password api route endpoint

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -10,4 +10,6 @@ router.post('/login', authController.login);
 
 router.post('/logout', authController.logout);
 
+router.post('/forgot-password', authController.forgotPassword);
+
 export default router;
