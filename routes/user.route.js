f897b82307ce0252378a23bc4f00550b0ededbac
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-17 20:04:20 +0100

    feat: add update current user api route endpoint

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,12 @@
 import express from 'express';
 
+import * as authMiddleware from '../middlewares/auth.middleware.js';
+import * as userController from '../controllers/user.controller.js';
+
 const router = express.Router();
+
+router.use(authMiddleware.protect);
+
+router.patch('/update-me', userController.updateMe);
 
 export default router;
