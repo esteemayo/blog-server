5926e6b94dae7d4124e5934e89487020928f145b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-17 19:35:33 +0100

    fix: modify

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
-import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
+import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/user.model.js';
