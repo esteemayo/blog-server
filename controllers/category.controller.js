8cc4311423bf54c2b65266123f31d683d385c0a0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-19 08:22:09 +0100

    feat: handle logic to fetch a single category endpoint

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -2,9 +2,26 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import Category from '../models/category.model.js';
+import { NotFoundError } from '../errors/not.found.error.js';
 
 export const getCategories = asyncHandler(async (req, res, next) => {
   const categories = await Category.find();
 
   return res.status(StatusCodes.OK).json(categories);
+});
+
+export const getCategory = asyncHandler(async (req, res, next) => {
+  const { id: categoryId } = req.params;
+
+  const category = await Category.findById(categoryId);
+
+  if (!category) {
+    return next(
+      new NotFoundError(
+        `There is no category found with the given ID → ${categoryId}`,
+      ),
+    );
+  }
+
+  return res.status(StatusCodes.OK).json(category);
 });
