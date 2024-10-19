878b55709f6d672daf4c0b63c51512d3d0c6d44f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-19 08:52:52 +0100

    fix: refactor

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,16 +1,8 @@
-import { StatusCodes } from 'http-status-codes';
-import asyncHandler from 'express-async-handler';
-
 import Category from '../models/category.model.js';
-import { NotFoundError } from '../errors/not.found.error.js';
 import * as factory from '../controllers/handler.factory.controller.js';
 
 export const getCategories = factory.getAll(Category);
-
 export const getCategory = factory.getOneById(Category);
-
 export const createCategory = factory.createOne(Category);
-
 export const updateCategory = factory.updateOne(Category);
-
 export const deleteCategory = factory.deleteOne(Category);
