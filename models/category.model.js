4e832e82e9eba16aad8386bc3ef6604e10d1ecc8
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-19 08:09:07 +0100

    feat: add category schema model

---
+++ models/category.model.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A category must have a name'],
+    },
+  },
+  { timestamps: true },
+);
+
+const Category =
+  mongoose.models.Category || mongoose.model('Category', categorySchema);
+
+export default Category;
