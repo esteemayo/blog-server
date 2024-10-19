8b3b1f71e29e4e1c15d68d74fb17abe91a64152d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-19 08:14:25 +0100

    feat: add fetch all categories api route endpoint

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,5 +1,14 @@
 import express from 'express';
 
+import * as authMiddleware from '../middlewares/auth.middleware.js';
+import * as categoryController from '../controllers/category.controller.js';
+
 const router = express.Router();
+
+router.use(authMiddleware.protect);
+
+router
+  .route('/')
+  .get(authMiddleware.restrictTo('admin'), categoryController.getCategories);
 
 export default router;
