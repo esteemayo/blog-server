6978938076043b631d9d42a73421fab273ee1463
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-21 13:27:18 +0100

    feat: handle logic to fetch a single post by id endpoint

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,9 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import Post from '../models/post.model.js';
+import * as factory from './handler.factory.controller.js';
 
 export const getPosts = asyncHandler(async (req, res, next) => {
   const posts = await Post.find();
 
   return res.status(StatusCodes.OK).json(posts);
 });
+
+export const getPostById = factory.getOneById(Post);
