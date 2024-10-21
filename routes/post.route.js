367d2f8addc340db0665ac76ca9b4fe78bf27279
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-21 13:18:07 +0100

    feat: add fetch all posts api route endpoint

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import * as postController from '../controllers/post.controller.js';
+
 const router = express.Router();
+
+router.route('/').get(postController.getPosts);
 
 export default router;
