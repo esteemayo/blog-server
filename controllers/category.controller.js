57af00ef7d15aaa16f19fd51452e7611336acd61
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-22 18:29:24 +0100

    fix: modify

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -2,7 +2,7 @@ import Category from '../models/category.model.js';
 import * as factory from '../controllers/handler.factory.controller.js';
 
 export const getCategories = factory.getAll(Category);
-export const getCategory = factory.getOneById(Category);
+export const getCategory = factory.getOneById(Category, 'category');
 export const createCategory = factory.createOne(Category);
-export const updateCategory = factory.updateOne(Category);
-export const deleteCategory = factory.deleteOne(Category);
+export const updateCategory = factory.updateOne(Category, 'category');
+export const deleteCategory = factory.deleteOne(Category, 'category');
