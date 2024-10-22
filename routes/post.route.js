70f322eeb712ab5789ffa6e1de41ec97493dbfa1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-22 16:43:47 +0100

    feat: add an endpoint to create a new post

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,10 +1,14 @@
 import express from 'express';
 
+import * as authMiddleware from '../middlewares/auth.middleware.js';
 import * as postController from '../controllers/post.controller.js';
 
 const router = express.Router();
 
-router.route('/').get(postController.getPosts);
+router
+  .route('/')
+  .get(postController.getPosts)
+  .post(authMiddleware.protect, postController.createPost);
 
 router.route('/:id').get(postController.getPostById);
 
