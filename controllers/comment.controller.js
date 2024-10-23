4922ef7304fdadb2a0d1c5d8d49b20bf4683e5a6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-23 18:17:05 +0100

    feat: handle fetch single comment logic

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -2,9 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import Comment from '../models/comment.model.js';
+import * as factory from './handler.factory.controller.js';
 
 export const getComments = asyncHandler(async (req, res, next) => {
   const comments = await Comment.find();
 
   return res.status(StatusCodes.OK).json(comments);
 });
+
+export const getComment = factory.getOneById(Comment, 'comment');
