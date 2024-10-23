fd9e706d2a607e7ccae42e42ad5905e22a209118
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-23 18:12:34 +0100

    feat: handle fetch all comments logic

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -2,3 +2,9 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import Comment from '../models/comment.model.js';
+
+export const getComments = asyncHandler(async (req, res, next) => {
+  const comments = await Comment.find();
+
+  return res.status(StatusCodes.OK).json(comments);
+});
