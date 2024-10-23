264ca7e35ab6b65320512669fd33508bbbdd10f4
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-24 00:58:57 +0100

    feat: create reply comment model schema

---
+++ models/reply.comment.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const { Types, Schema } = mongoose;
+
+const replyCommentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      trim: true,
+      required: [true, 'A reply must have a content'],
+    },
+    comment: {
+      type: Types.ObjectId,
+      ref: 'Comment',
+      required: [true, 'A reply must have a comment'],
+    },
+    post: {
+      type: Types.ObjectId,
+      ref: 'Post',
+      required: [true, 'A reply must belong to a post'],
+    },
+    author: {
+      type: Types.ObjectId,
+      ref: 'User',
+      required: [true, 'A reply must belong to an author'],
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const ReplyComment =
+  mongoose.models.ReplyComment ||
+  mongoose.model('ReplyComment', replyCommentSchema);
+
+export default ReplyComment;
