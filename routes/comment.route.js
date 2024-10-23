a78a6b133a212e72f4d9c0c5705fa666eb737fe5
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-23 18:10:16 +0100

    feat: add endpoint to fetch all comments

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
 
-import * as authMiddleware from '../middlewares/auth.middleware';
+import * as authMiddleware from '../middlewares/auth.middleware.js';
+import * as commentController from '../controllers/comment.controller.js';
 
 const router = express.Router();
+
+router.route('/').get(commentController.getComments);
 
 export default router;
