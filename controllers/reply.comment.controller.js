8038b702f756499984ecdd75a1c0e8fc97c67461
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-24 15:50:56 +0100

    feat: create reply comment controller

---
+++ controllers/reply.comment.controller.js
@@ -0,0 +1,6 @@
+import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+
+import ReplyComment from '../models/reply.comment.model.js';
+
+export const getComments = asyncHandler(async (req, res, next) => {});
