3576e72344748a335db0fdb479348d514f65d8fb
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-24 15:19:42 +0100

    fix: modify

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -3,7 +3,7 @@ import express from 'express';
 import * as authMiddleware from '../middlewares/auth.middleware.js';
 import * as commentController from '../controllers/comment.controller.js';
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
