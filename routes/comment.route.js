efad646571a19a467ff1307d1babc27ff589a993
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-25 01:33:12 +0100

    fix: merge params

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import replyCommentRoute from './reply.comment.route.js';
+
 import * as authMiddleware from '../middlewares/auth.middleware.js';
 import * as commentController from '../controllers/comment.controller.js';
 
 const router = express.Router({ mergeParams: true });
+
+router.use('/:commentId/reply-comments', replyCommentRoute);
 
 router
   .route('/')
