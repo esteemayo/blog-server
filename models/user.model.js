44719d3046f68a14860098d73df001769d75032b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-28 17:54:53 +0100

    fix: refactor

--- models/user.model.js
+++ models/user.model.js
@@ -32,10 +32,14 @@ const userSchema = new Schema(
       unique: true,
       trim: true,
       lowerCase: true,
-      match: [
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\0-9]+\.)+[a-zA-Z]{2,}))$/,
-        'Please enter a valid email address',
-      ],
+      validate: {
+        validator: function (val) {
+          const emailRegex =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return emailRegex.test(val);
+        },
+        message: 'Please enter a valid email address',
+      },
     },
     password: {
       type: String,
