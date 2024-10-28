1f858e7497316c46997a8053fe8ba07ecf2898f9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-28 17:35:35 +0100

    fix: modify

--- models/user.model.js
+++ models/user.model.js
@@ -19,7 +19,6 @@ const userSchema = new Schema(
       required: [true, 'Please provide your username'],
       unique: true,
       trim: true,
-      match: [/^[a-zA-Z0-9_]+$/, 'Username cannot contain special characters'],
       validate: {
         validator: function (val) {
           return /^[a-zA-Z0-9_]+$/.test(val);
